1a25b6c034fd65c2def266e42fa80f02eb6ac7cf
midorigd <lo.carson.m@gmail.com>
2026-04-05 13:28:08 -0400

    Added one-bit branch predictor

---
+++ simulator/branch_predictor/one_bit.py
@@ -0,0 +1,12 @@
+class OneBitPredictor:
+    def __init__(self):
+        self.prediction = False
+        self.mispredicts = 0
+        self.total = 0
+
+    def predict(self, actual):
+        if self.prediction != actual:
+            self.mispredicts += 1
+            self.prediction = actual
+
+        self.total += 1
